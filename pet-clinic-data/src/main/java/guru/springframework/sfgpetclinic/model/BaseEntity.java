fecb294ee21c72ef3e8b08045763115baa6a3a9d
Alkan <alkanorgun@gmail.com>
2021-01-16 00:26:38 +0300

    Adding base entity. Closes #10.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long id; // box type is recommended in hibernate as boxed values can be null
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
