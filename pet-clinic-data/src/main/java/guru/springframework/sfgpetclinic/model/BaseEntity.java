5d219cc49b9e343ee64bb3a5223ee6fac5465f66
Alkan <alkanorgun@gmail.com>
2021-01-30 22:13:37 +0300

    Updated base entity with JPA annotatinos. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // sequence also cool, but your sql choice must implement that
     private Long id; // box type is recommended in hibernate as boxed values can be null
 
     public Long getId() {
